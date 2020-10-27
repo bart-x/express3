a9d4b7c968256b14928fa56e5ccaddb0c0ef19b7
Bartek K <kontener00@gmail.com>
2020-10-27 21:12:29 +0100

    update server file

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.json());
 
 app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
 app.use('/api/', seatsRoutes);
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
