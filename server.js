67a90115c237090b0fff018daa9842e39a247ea8
Bartek K <kontener00@gmail.com>
2020-11-04 19:41:51 +0100

    added mongo atlas configuration

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use((req, res) => {
 });
 
 // connects our backend code with the database
-mongoose.connect("mongodb://localhost:27017/NewWaveDB", {
-    useNewUrlParser: true,
+mongoose.connect("mongodbmongodb+srv://power_user:<password>@cluster0concert.wascp.mongodb.net/<NewWaveDB>?retryWrites=true&w=majority", {
+    useNewUrlParser: true, useUnifiedTopology: true
 });
 const db = mongoose.connection;
 
